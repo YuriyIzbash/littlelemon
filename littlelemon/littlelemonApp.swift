0a57d583443a8e2f5021e75de105513d4e1196f2
Yuriy Izbash <izbash91@gmail.com>
2025-09-04 18:19:28 +0200

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by yuriy on 4. 9. 25.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ littlelemonTests/littlelemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonTests.swift
+//  littlelemonTests
+//
+//  Created by yuriy on 4. 9. 25.
+//
+
+import Testing
+@testable import littlelemon
+
+struct littlelemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
